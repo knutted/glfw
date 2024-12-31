9d7799a77c09d7c6626cfd25307066efc38bae15
knutted <drewh2os@gmail.com>
2024-12-30 23:06:52 -0500

    updated premake5 configuration

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,11 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "off"
+	warnings "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("dist/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("dist/intermediates/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -14,21 +16,29 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
 	}
+
 	filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
-		staticruntime "On"
-
+		
 		files
 		{
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
+			"src/posix_module.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
 			"src/glx_context.c",
@@ -42,14 +52,36 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
+	filter "system:macosx"
+		pic "On"
+
+		files
+		{
+			"src/cocoa_init.m",
+			"src/cocoa_monitor.m",
+			"src/cocoa_window.m",
+			"src/cocoa_joystick.m",
+			"src/cocoa_time.c",
+			"src/nsgl_context.m",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/osmesa_context.c",
+			"src/egl_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_COCOA"
+		}
+
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
@@ -69,6 +101,17 @@ project "GLFW"
 		runtime "Debug"
 		symbols "on"
 
+	filter { "system:windows", "configurations:Debug-AS" }	
+		runtime "Debug"
+		symbols "on"
+		sanitize { "Address" }
+		flags { "NoRuntimeChecks", "NoIncrementalLink" }
+
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "speed"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "speed"
+        symbols "off"
